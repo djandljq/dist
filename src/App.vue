77ebfa076b4bbd652d5017dc54429f6a6e34ac7f
djandljq <373761027@qq.com>
2018-01-11 18:11:07 +0800

    11

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+  	<div class="search">
+  		<span class="iconfont"></span>
+  	</div>
+   <swiper :list="list" auto style="width:100%;margin:0 auto;" height="200px" :loop="true"></swiper>
+  </div>
+</template>
+
+<script>
+	import { Swiper } from 'vux'
+export default {
+  name: 'app',
+  components:{
+  	Swiper
+  },
+  data(){
+  	return {
+  		list:[{
+			  img: '../static/img/lunbo.jpg',
+			}, {
+			  img: '../static/img/lunbo1.jpg',
+			}, {
+			  fallbackImg: '../static/img/lunbo02.jpg'
+			}]
+  	}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+html,body{
+	width: 100%;
+	height: 100%;
+	margin: 0;
+}
+</style>
